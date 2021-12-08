91888af7b825dd3b5da8bb168b8aa0a5a4151491
Samuel Gerstein <sgerstein2019@fau.edu>
2021-12-07 19:08:11 -0500

    finished dynamic sidebar for visualization

--- server.R
+++ server.R
@@ -10,26 +10,38 @@
 source("global.R")
 library(shiny)
 library(tidyverse)
-data <- read.csv("https://raw.githubusercontent.com/samuel-gerstein/WHO-Life-Expectancy/main/Life%20Expectancy%20Data.csv", header = TRUE)
+data <-
+  read.csv(
+    "https://raw.githubusercontent.com/samuel-gerstein/WHO-Life-Expectancy/main/Life%20Expectancy%20Data.csv",
+    header = TRUE
+  )
 # Define server logic required to draw a histogram
 country_name <- data$Country
 
 server <- function(input, output, session) {
-
   ## Tab 1
-  updateSelectizeInput(session, "country_bar", choices = country_name, server = TRUE)
+  updateSelectizeInput(session,
+                       "country_bar",
+                       choices = country_name,
+                       server = TRUE)
   country_data <- reactive({
-    if(!is.null(input$country_bar) & !is.null(input$year_bar))
+    if (!is.null(input$country_bar) & !is.null(input$year_bar))
     {
-      return(subset(data, country_name %in%input$country_bar & data$Year %in% input$year_bar))
+      return(
+        subset(
+          data,
+          country_name %in% input$country_bar &
+            data$Year %in% input$year_bar
+        )
+      )
     }
-    else if(!is.null(input$country_bar))
+    else if (!is.null(input$country_bar))
     {
-      return(subset(data, country_name %in%input$country_bar))
+      return(subset(data, country_name %in% input$country_bar))
     }
     else
     {
-      return(subset(data, data$Year %in%input$year_bar))
+      return(subset(data, data$Year %in% input$year_bar))
     }
   })
   output$CountryTable <- renderTable(country_data())
@@ -37,88 +49,142 @@ server <- function(input, output, session) {
   choice_data <- reactive({
     if (input$group_bar == "Year" & input$statistic_bar == "Mean")
     {
-      return(
-        data %>%
-          group_by(Year) %>%
-          select(-c("Status","Country")) %>%
-          summarize_all(mean,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Year) %>%
+               select(-c("Status", "Country")) %>%
+               summarize_all(mean, na.rm = TRUE))
     }
     
-    else if (input$group_bar == "Year" & input$statistic_bar == "Median")
+    else if (input$group_bar == "Year" &
+             input$statistic_bar == "Median")
     {
-      return(
-        data %>%
-          group_by(Year) %>%
-          select(-c("Status","Country")) %>%
-          summarize_all(median,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Year) %>%
+               select(-c("Status", "Country")) %>%
+               summarize_all(median, na.rm = TRUE))
     }
-    else if (input$group_bar == "Year" & input$statistic_bar == "Standard Deviation")
+    else if (input$group_bar == "Year" &
+             input$statistic_bar == "Standard Deviation")
     {
-      return(
-        data %>%
-          group_by(Year) %>%
-          select(-c("Status","Country")) %>%
-          summarize_all(sd,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Year) %>%
+               select(-c("Status", "Country")) %>%
+               summarize_all(sd, na.rm = TRUE))
     }
-    if (input$group_bar == "Country" & input$statistic_bar == "Mean")
+    if (input$group_bar == "Country" &
+        input$statistic_bar == "Mean")
+    {
+      return(data %>%
+               group_by(Country) %>%
+               select(-c("Status", "Year")) %>%
+               summarize_all(mean, na.rm = TRUE))
+    }
+    if (input$group_bar == "Country" &
+        input$statistic_bar == "Median")
     {
       return(
         data %>%
           group_by(Country) %>%
-          select(-c("Status","Year")) %>%
-          summarize_all(mean,na.rm=TRUE)
+          select(-c("Status", "Year")) %>%
+          summarize_all(median, na.rm = TRUE)
       )
     }
-    if (input$group_bar == "Country" & input$statistic_bar == "Median")
+    if (input$group_bar == "Country" &
+        input$statistic_bar == "Standard Deviation")
     {
-      return(
-        data %>%
-          group_by(Country) %>%
-          select(-c("Status","Year")) %>%
-          summarize_all(median,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Country) %>%
+               select(-c("Status", "Year")) %>%
+               summarize_all(sd, na.rm = TRUE))
     }
-    if (input$group_bar == "Country" & input$statistic_bar == "Standard Deviation")
+    if (input$group_bar == "Development" &
+        input$statistic_bar == "Mean")
     {
-      return(
-        data %>%
-          group_by(Country) %>%
-          select(-c("Status","Year")) %>%
-          summarize_all(sd,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Status) %>%
+               select(-c("Country", "Year")) %>%
+               summarize_all(mean, na.rm = TRUE))
     }
-    if (input$group_bar == "Development" & input$statistic_bar == "Mean")
+    if (input$group_bar == "Development" &
+        input$statistic_bar == "Median")
     {
-      return(
-        data %>%
-          group_by(Status) %>%
-          select(-c("Country","Year")) %>%
-          summarize_all(mean,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Status) %>%
+               select(-c("Country", "Year")) %>%
+               summarize_all(median, na.rm = TRUE))
     }
-    if (input$group_bar == "Development" & input$statistic_bar == "Median")
+    if (input$group_bar == "Development" &
+        input$statistic_bar == "Standard Deviation")
     {
-      return(
-        data %>%
-          group_by(Status) %>%
-          select(-c("Country","Year")) %>%
-          summarize_all(median,na.rm=TRUE)
-      )
-    }
-    if (input$group_bar == "Development" & input$statistic_bar == "Standard Deviation")
-    {
-      return(
-        data %>%
-          group_by(Status) %>%
-          select(-c("Country","Year")) %>%
-          summarize_all(sd,na.rm=TRUE)
-      )
+      return(data %>%
+               group_by(Status) %>%
+               select(-c("Country", "Year")) %>%
+               summarize_all(sd, na.rm = TRUE))
     }
   })
   
   output$StatsTable <- renderTable(choice_data())
   ## Tab 2
+  observe({
+    if (input$x_bar == "Life Expectancy")
+    {
+      updateSelectizeInput(
+        session,
+        "y_bar",
+        choices = c("BMI", "Development"),
+        server = TRUE
+      )
+      observe({
+        if (input$y_bar == "BMI")
+        {
+          updateCheckboxGroupInput(session, "z_bar", choices = c("Development"))
+        }
+        else {
+          updateCheckboxGroupInput(session, "z_bar", choices = c("BMI"))
+        }
+      })
+    }
+  })
+  observe({
+    if (input$x_bar == "BMI")
+    {
+      updateSelectizeInput(
+        session,
+        "y_bar",
+        choices = c("Life Expectancy", "Development"),
+        server = TRUE
+      )
+      observe({
+        if (input$y_bar == "Life Expectancy")
+        {
+          updateCheckboxGroupInput(session, "z_bar", choices = c("Development"))
+        }
+        else {
+          updateCheckboxGroupInput(session, "z_bar", choices = c("Life Expectancy"))
+        }
+      })
+    }
+  })
+  observe({
+    if (input$x_bar == "Development")
+    {
+      updateSelectizeInput(
+        session,
+        "y_bar",
+        choices = c("BMI", "Life Expectancy"),
+        server = TRUE
+      )
+      observe({
+        if (input$y_bar == "BMI")
+        {
+          updateCheckboxGroupInput(session, "z_bar", choices = c("Life Expectancy"))
+        }
+        else {
+          updateCheckboxGroupInput(session, "z_bar", choices = c("BMI"))
+        }
+      })
+    }
+  })
+  
+  output$visualization <- 
 }
--- ui.R
+++ ui.R
@@ -12,29 +12,69 @@ library(shinythemes)
 navbarPage(
   theme = shinytheme("flatly"),
   "WHO Life Expectancy Analysis",
-  tabPanel("Descriptive Statistics", 
+  tabPanel("Descriptive Statistics",
            tabsetPanel(
-            tabPanel("Filter by Country/Year",
-                     sidebarPanel(
-                       selectizeInput('country_bar',h2("Country"), choices=NULL, multiple=TRUE),
-                       selectInput('year_bar', h2('Year'),
-                                   choices = c(2000:2015), selectize = TRUE, multiple = TRUE)
-                     ),
-                    style='overflow: scroll', tableOutput("CountryTable")
-                    ),
-            tabPanel("Summary Statistics by Country, Year, Development",
-                     sidebarPanel(
-                       selectizeInput('group_bar',h3("Choose Variable to Group By:"), choices=c("Country","Year","Development"), multiple=FALSE),
-                       selectizeInput('statistic_bar',h3("Choose Statistic"), choices=c("Mean", "Median", "Standard Deviation"), multiple=FALSE)
-                     ),
-                     
-                     tableOutput("StatsTable")
-                     )
-           )
-           ),
-  tabPanel("Visualization",
-           
-           ),
+             tabPanel(
+               "Filter by Country/Year",
+               sidebarPanel(
+                 selectizeInput(
+                   'country_bar',
+                   h2("Country"),
+                   choices = NULL,
+                   multiple = TRUE
+                 ),
+                 selectInput(
+                   'year_bar',
+                   h2('Year'),
+                   choices = c(2000:2015),
+                   selectize = TRUE,
+                   multiple = TRUE
+                 )
+               ),
+               style = 'overflow: scroll',
+               tableOutput("CountryTable")
+             ),
+             tabPanel(
+               "Summary Statistics by Country, Year, Development",
+               sidebarPanel(
+                 selectizeInput(
+                   'group_bar',
+                   h3("Choose Variable to Group By:"),
+                   choices = c("Country", "Year", "Development"),
+                   multiple = FALSE
+                 ),
+                 selectizeInput(
+                   'statistic_bar',
+                   h3("Choose Statistic"),
+                   choices = c("Mean", "Median", "Standard Deviation"),
+                   multiple = FALSE
+                 )
+               ),
+               
+               tableOutput("StatsTable")
+             )
+           )),
+  tabPanel(
+    "Visualization",
+    sidebarPanel(
+      selectizeInput(
+        'x_bar',
+        h3("Choose x-variable"),
+        choices = c("Life Expectancy", "BMI", "Development"),
+        multiple = FALSE
+      ),
+      
+      selectizeInput(
+        'y_bar',
+        h3("Choose y-variable"),
+        choices = NULL,
+        multiple = FALSE
+      ),
+      
+      checkboxGroupInput('z_bar', h3("Choose optional variable"), choices =
+                           NULL)
+    )
+  ),
   tabPanel("Staistical Analysis", "contents3")
-  )
+)
 
